067e89c99a7cd8df56a7e03e54e75c6cae400d55
SivanMiller <75025199+SivanMiller@users.noreply.github.com>
2021-05-29 13:13:38 +0300

    tasks- start

--- UI/src/tasks/FileTask.java
+++ UI/src/tasks/FileTask.java
@@ -6,13 +6,18 @@ public class FileTask extends Task<Boolean> {
 
     @Override
     protected Boolean call() throws Exception {
+        updateProgress(0,100);
         Thread.sleep(1000);
+        updateProgress(20,100);
         updateMessage("Fetching file..");
         Thread.sleep(1000);
+        updateProgress(40,100);
         updateMessage("Fetching stocks..");
         Thread.sleep(1000);
+        updateProgress(60,100);
         updateMessage("Fetching users..");
         Thread.sleep(1000);
+        updateProgress(80,100);
         updateMessage("File Loaded!");
         updateProgress(100,100);
         return Boolean.TRUE;
