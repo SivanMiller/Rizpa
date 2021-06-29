1c79bf9562c1b3ef6bf02f821e931686031c3488
SivanMiller <75025199+SivanMiller@users.noreply.github.com>
2021-06-29 19:01:27 +0300

    START

--- Engine/src/engine/Engine.java
+++ Engine/src/engine/Engine.java
@@ -43,6 +43,7 @@ public class Engine implements RizpaMethods {
 
 
 
+
     //Converting XML Data with JAXB
     private static RizpaStockExchangeDescriptor deserializeFrom(InputStream in, String FileName) throws JAXBException {
         JAXBContext jc = JAXBContext.newInstance(JAXB_XML_GAME_PACKAGE_NAME);
