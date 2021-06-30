92b8700cce4329735dc17cbe19e69179dc28dc20
SivanMiller <75025199+SivanMiller@users.noreply.github.com>
2021-06-30 19:46:22 +0300

    START

---
+++ Engine/src/engine/StockManager.java
@@ -0,0 +1,33 @@
+package engine;
+
+import exception.StockNegPriceException;
+import exception.StockSymbolLowercaseException;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class StockManager {
+    private Map<String, Stock> Stocks;
+
+    public StockManager() {
+        Stocks = new HashMap<>();
+    }
+
+    public synchronized void addStock(String Symbol, String CompanyName, int Price) throws StockSymbolLowercaseException, StockNegPriceException {
+        Stock newStock = new Stock(CompanyName, Symbol, Price);
+        Stocks.put(Symbol, newStock);
+    }
+
+    public synchronized void removeStock(String Symbol) {
+        Stocks.remove(Symbol);
+    }
+
+    public synchronized Map<String, Stock> getStocks() {
+        return Collections.unmodifiableMap(Stocks);
+    }
+
+    public boolean isStockExists(String Symbol) {
+        return Stocks.containsKey(Symbol);
+    }
+}
